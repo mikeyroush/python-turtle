f6b8ee836fcb0401c5cb8e73013e76629992dc4f
Mikey Roush <mikeyroush@Mikeys-MacBook-Pro.local>
2020-05-05 21:50:27 -0500

    made turtle race function

--- turt.py
+++ turt.py
@@ -1,4 +1,5 @@
 import turtle
+import math
 from random import choice, randrange
 
 
@@ -32,20 +33,47 @@ def right():
     tim.forward(15)
 
 
+def race(turtleNum):
+    turtleNum = min(turtleNum, len(colors))
+    turtles = []
+    start = turtle.window_width()/-4
+    dx = turtle.window_width()/2/(turtleNum-1)
+
+    for i in range(turtleNum):
+        turt = turtle.Turtle()
+        color = choice(colors)
+        colors.remove(color)
+        turt.shape('turtle')
+        turt.color(color)
+        turt.penup()
+        turt.setheading(90)
+        turt.setpos((start + i * dx, start))
+        turt.pendown()
+        turtles.append(turt)
+
+    speeds = [randrange(10, 15) for _ in range(turtleNum)]
+
+    for _ in range(math.floor(turtle.window_height()/2/max(speeds))):
+        for index, turt in enumerate(turtles):
+            turt.forward(speeds[index])
+
+
 TK_SILENCE_DEPRECATION = 1
-tim = turtle.Turtle()
-colors = "maroon blue red gray brown tan pink orange yellow"
+colors = "maroon blue red gray brown tan pink orange green"
 colors = colors.split()
-tim.shape('turtle')
-tim.color('green')
-tim.speed(5000)
+# tim = turtle.Turtle()
+# tim.shape('turtle')
+# tim.color('green')
+# tim.speed(5000)
 
 # drawShapes(100)
-turtle.listen()
 
-turtle.onkey(up, 'Up')
-turtle.onkey(down, 'Down')
-turtle.onkey(left, 'Left')
-turtle.onkey(right, 'Right')
+# turtle.listen()
+# turtle.onkey(up, 'Up')
+# turtle.onkey(down, 'Down')
+# turtle.onkey(left, 'Left')
+# turtle.onkey(right, 'Right')
+
+race(5)
 
 turtle.done()
